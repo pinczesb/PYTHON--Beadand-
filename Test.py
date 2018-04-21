16f076fffddfaaa5fa7891bde2e8a510be8f5802
pinczesb <36756180+pinczesb@users.noreply.github.com>
2018-04-21 12:31:11 +0200

    Add files via upload

--- Test.py
+++ Test.py
@@ -18,3 +18,18 @@ def secondsToTime(n,outFile):
     min = n//60
     sec = n%60
     print('{} seconds is {} year {} day {} hours {} minutes and {} seconds'.format(tmp,year,day,hour,min,sec))
+
+def sumOfNumbers(outFile):
+    sum = 0;
+    for n in outFile:
+        sum+=int(n)
+    return sum
+
+def sumOfFirstN(n,outFile):
+    sum = 0
+    s=''
+    for i in range(1,n+1):
+        s += str(i) + '+'
+        sum += i
+    s = s[0:-1] + '=' + str(sum)
+    print(s,file=outFile)
