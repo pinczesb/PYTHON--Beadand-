66a5fad2df5911ad72ef85cfd363b7d72d79e2b8
pinczesb <36756180+pinczesb@users.noreply.github.com>
2018-04-25 21:37:11 +0200

    Add files via upload

--- 6-MORSE.py
+++ 6-MORSE.py
@@ -1,6 +1,7 @@
 import string
 
 DICT_Morse_UpperAndNumbers={'A': '.-', 'B': '-...', 'C': '-.-.','D': '-..', 'E': '.', 'F': '..-.','G': '--.', 'H': '....', 'I': '..','J': '.---', 'K': '-.-', 'L': '.-..','M': '--', 'N': '-.', 'O': '---','P': '.--.', 'Q': '--.-', 'R': '.-.','S': '...', 'T': '-', 'U': '..-','V': '...-', 'W': '.--', 'X': '-..-','Y': '-.--', 'Z': '--..','0': '-----', '1': '.----', '2': '..---','3': '...--', '4': '....-', '5': '.....','6': '-....', '7': '--...', '8': '---..','9': '----.',' ':'/'}
+DICT_UpperAndNumbers_Morse = DICT_Morse_UpperAndNumbers([(value,key) for key,value in old_dict.items()])
 
 def ConvertToMorse():
     str2=''
@@ -11,4 +12,12 @@ def ConvertToMorse():
     for ch2 in str2:
         print(DICT_Morse_UpperAndNumbers[ch2.upper()],'/',end='')
 
-ConvertToMorse()
+def ConverFromMorse():
+    str2=''
+    str=ConvertToMorse()
+    for ch in str:
+        str2+=ch
+    for ch2 in str2:
+        print(DICT_Morse_UpperAndNumbers[])
+
+
