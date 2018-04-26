34f39961dc73577512e57bf7cfb31fb39b9aa376
pinczesb <36756180+pinczesb@users.noreply.github.com>
2018-04-26 18:48:41 +0200

    Add files via upload

--- 6-MORSE.py
+++ 6-MORSE.py
@@ -1,23 +1,23 @@
 import string
 
 DICT_Morse_UpperAndNumbers={'A': '.-', 'B': '-...', 'C': '-.-.','D': '-..', 'E': '.', 'F': '..-.','G': '--.', 'H': '....', 'I': '..','J': '.---', 'K': '-.-', 'L': '.-..','M': '--', 'N': '-.', 'O': '---','P': '.--.', 'Q': '--.-', 'R': '.-.','S': '...', 'T': '-', 'U': '..-','V': '...-', 'W': '.--', 'X': '-..-','Y': '-.--', 'Z': '--..','0': '-----', '1': '.----', '2': '..---','3': '...--', '4': '....-', '5': '.....','6': '-....', '7': '--...', '8': '---..','9': '----.',' ':'/'}
-DICT_UpperAndNumbers_Morse = DICT_Morse_UpperAndNumbers([(value,key) for key,value in old_dict.items()])
+DICT_UpperAndNumbers_Morse={v:k for k, v in DICT_Morse_UpperAndNumbers.items()}
 
-def ConvertToMorse():
+str=input('String to convert: ')
+
+def ConvertToMorse(str):
     str2=''
-    str=input('String to convert: ')
     for ch in str:
         if ch not in string.punctuation:
-            str2+=ch
+            str2+=ch+'/'
     for ch2 in str2:
         print(DICT_Morse_UpperAndNumbers[ch2.upper()],'/',end='')
 
-def ConverFromMorse():
-    str2=''
-    str=ConvertToMorse()
-    for ch in str:
-        str2+=ch
-    for ch2 in str2:
-        print(DICT_Morse_UpperAndNumbers[])
+def ConvertFromMorse(str):
+    str2=ConvertToMorse(str)
+    for ch in str2:
+        print(DICT_UpperAndNumbers_Morse[ch],end='')
 
 
+
+ConvertFromMorse(str)
