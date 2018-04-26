c92bb0ca7c50b7783223f9b04fca2c5fddfcf297
pinczesb <36756180+pinczesb@users.noreply.github.com>
2018-04-26 18:49:52 +0200

    Add files via upload

--- 6-MORSE.py
+++ 6-MORSE.py
@@ -6,18 +6,14 @@ DICT_UpperAndNumbers_Morse={v:k for k, v in DICT_Morse_UpperAndNumbers.items()}
 str=input('String to convert: ')
 
 def ConvertToMorse(str):
+    outfile=open('C:\\Users\Bence\Desktop\python\python.txt','w')
     str2=''
     for ch in str:
         if ch not in string.punctuation:
-            str2+=ch+'/'
+            str2+=ch
     for ch2 in str2:
-        print(DICT_Morse_UpperAndNumbers[ch2.upper()],'/',end='')
+        print(DICT_Morse_UpperAndNumbers[ch2.upper()], '/', end='')
+        print(DICT_Morse_UpperAndNumbers[ch2.upper()],'/',end='',file=outfile)
+    outfile.close()
 
-def ConvertFromMorse(str):
-    str2=ConvertToMorse(str)
-    for ch in str2:
-        print(DICT_UpperAndNumbers_Morse[ch],end='')
-
-
-
-ConvertFromMorse(str)
+ConvertToMorse(str)
