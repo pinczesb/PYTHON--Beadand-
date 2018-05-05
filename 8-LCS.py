1ccd757caec8661f59f3d820a870bbd90f3793d0
pinczesb <36756180+pinczesb@users.noreply.github.com>
2018-05-05 15:58:15 +0200

    Add files via upload

--- 8-LCS.py
+++ 8-LCS.py
@@ -1,1 +1,19 @@
-def LLS(s1,s2):
+def LongestCommonSubsequence(str1,str2):
+    idx=0
+    lcs=""
+    if len(str1)>len(str2):
+        maxlen=str1
+    if len(str1)<len(str2):
+        maxlen=str2
+    else:
+        maxlen=str1
+    while idx<len(maxlen):
+        if str1[idx] is str2[idx]:
+            lcs+=maxlen[idx]
+        return lcs
+    idx+=1
+
+
+s1="aaafghl"
+s2="aaakgh"
+print(LongestCommonSubsequence(s1,s2))
