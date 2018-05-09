c12fa06cf3db18b95fe106e68fdca4480edd6ae8
pinczesb <36756180+pinczesb@users.noreply.github.com>
2018-05-09 13:58:41 +0200

    Add files via upload

--- 8-LCS.py
+++ 8-LCS.py
@@ -1,23 +1,24 @@
-def LCS(x, y):
-    if len(x)>len(y):
-        max=len(x)
-    else:
-        max=len(y)
-    idx=max-1
-    while idx!=-1:
-        if len(x) == 0 or len(y) == 0:
-           return ""
-        if x[idx] == y[idx]:
-            return x[idx] + LCS(x[idx+1], y[idx+1])
-        else:
-            a = LCS(x[idx+1], y)
-            b = LCS(x, y[idx+1])
-
-            if len(a) > len(b):
-                return a
+def lcs(x, y):
+    for idx1 in range(len(x)-1):
+        for idx2 in range(len(y)-1):
+            if len(x) == 0 or len(y) == 0:
+               return ""
+            if x[idx1] == y[idx2]:
+                return x[idx1] + lcs(x[idx1], y[idx2])
             else:
-                return b
+                a = lcs(x[idx1], y)
+                b = lcs(x, y[idx2])
+                if len(a) > len(b):
+                    return a
+                else:
+                    return b
 
+s1='aggtab'
+s2='gxtxayb'
+
+print(lcs(s1,s2))
+print(len(s1))
+print(len(s2))
 s1='aggtab'
 s2='gxtxayb'
 
