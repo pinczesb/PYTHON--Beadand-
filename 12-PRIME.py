f472ec99e4aa931ab4b6063acf368bf2c8cae936
pinczesb <36756180+pinczesb@users.noreply.github.com>
2018-05-10 20:53:37 +0200

    Add files via upload

--- 12-PRIME.py
+++ 12-PRIME.py
@@ -1,12 +1,21 @@
+def isPrime(n):
+    if n==1:
+        return False
+    elif n==2:
+        return True
+    else:
+        for i in range(2,n):
+            if(n%i==0):
+                return False
+        return True
+
 def nthPrime(n):
-    L=[]
-    for num in range(2,20):
-       if num > 1:
-           for i in range(2,num):
-               if (num % i) == 0:
-                   break
-           else:
-               L.append(num)
-    return L[n-1]
+    sum=0
+    prime=1
+    while sum<n:
+        prime+=1
+        if isPrime(prime):
+            sum+=1
+    return prime
 
 print(nthPrime(6))
